cd93f234c10414710d5aeeda0f4b75d79ec464a6
OwlAford <1005476098@qq.com>
2017-01-10 21:59:00 +0800

    update sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -6,63 +6,163 @@ export default class Sidebar extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      sub1: false,
-      sub2: false
+      menu: [
+              {
+                active: true,
+                title: '测试条目',
+                titleIcon: 'organization',
+                subList: [
+                  {
+                    href: '/inmanage/main/message',
+                    name: '参考模板'
+                  }
+                ]
+              }, {
+                active: false,
+                title: '机构管理',
+                titleIcon: 'organization',
+                subList: [
+                  {
+                    href: '/inmanage/main/branchManage',
+                    name: '机构管理'
+                  }
+                ]
+              }, {
+                active: false,
+                title: '用户管理',
+                titleIcon: 'userManage',
+                subList: [
+                  {
+                    href: '/inmanage/main/userManage',
+                    name: '用户管理'
+                  }
+                ]
+              }, {
+                active: false,
+                title: '角色管理',
+                titleIcon: 'roleManage',
+                subList: [
+                  {
+                    href: '/inmanage/main/roleManage',
+                    name: '角色管理'
+                  }
+                ]
+              }, {
+                active: false,
+                title: '岗位管理',
+                titleIcon: 'postManage',
+                subList: [
+                  {
+                    href: '/inmanage/main/postManage',
+                    name: '岗位管理'
+                  }
+                ]
+              }, {
+                active: false,
+                title: '策略管理',
+                titleIcon: 'strategyManage',
+                subList: [
+                  {
+                    href: '/inmanage/main/reviewSetting',
+                    name: '审查设置'
+                  }, {
+                    href: '/inmanage/main/strategySetting',
+                    name: '策略设置'
+                  }
+                ]
+              }, {
+                active: false,
+                title: '审查管理',
+                titleIcon: 'reviewManage',
+                subList: [
+                  {
+                    href: '/inmanage/main/approveList',
+                    name: '审批列表'
+                  }, {
+                    href: '/inmanage/main/approveHistory',
+                    name: '审批历史'
+                  }, {
+                    href: '/inmanage/main/approveRecord',
+                    name: '审批记录'
+                  }
+                ]
+              },
+            ]
     }
   }
 
-  handleSubActive(e) {
-    let target = e.currentTarget.getAttribute('data-target')
-    this.setState({
-      [target]: !this.state[target]
-    })
+  handleSubActive(e, index) {
+    let state = Object.assign({}, this.state)
+    let current = state.menu[index].active
+    state.menu[index].active = !current
+    this.setState(state)
   }
 
   render() {
+    const menuListClass = (index) => {
+      return this.state.menu[index].active ? 'menuList active' : 'menuList'
+    }
+
+    const titleClass = (index) => {
+      return this.state.menu[index].active ? 'title active' : 'title'
+    }
+
+    const Title = (name, icon, index) => {
+      return (
+        <div 
+          className={titleClass(index)} 
+          onClick={(e) => this.handleSubActive(e, index)}
+        >
+          <i className={icon}></i>
+          {name}
+        </div>
+      )
+    }
+
+    const MenuList = ({ subList, index }) => {
+      return (
+        <div className={menuListClass(index)}>
+          {subList.map(
+            (item, i) => {
+              return (
+                <div key={i} className="item">
+                  <Link to={item.href} activeClassName='active'>{item.name}</Link>
+                </div>
+              ) 
+            }
+          )}
+        </div>
+      )
+    }
+
+    const SubMenu = ({title, titleIcon, subList}, index) => {
+      return (
+        <div className="subMenu">
+          {Title(title, titleIcon, index)}
+          {MenuList({subList: subList, index})}
+        </div>
+      )
+    }
+
+    const Menu = (dataArray) => {
+      return (
+        <div className="menu">
+          {dataArray.map(
+            (item, i) => {
+              return (
+                <div key={i}>
+                  {SubMenu(item, i)}
+                </div>
+              ) 
+            }
+          )}
+        </div>
+      )
+    }
+
     return (
       <div className="app-sidebar">
-        <div className="menu">
-          <div className="subMenu">
-            <div 
-              className={this.state.sub1 ? 'title active' : 'title'} 
-              data-target="sub1"
-              onClick={(e) => this.handleSubActive(e)}
-            >
-              机构管理
-            </div>
-            <div className={this.state.sub1 ? 'menuList active' : 'menuList'}>
-              <div className="item">
-                <Link to='/inmanage/main/branchManage' activeClassName='active'>机构管理</Link>
-              </div>
-            </div>
-          </div>
-          <div className="subMenu">
-            <div className="title" 
-              className={this.state.sub2 ? 'title active' : 'title'} 
-              data-target="sub2"
-              onClick={(e) => this.handleSubActive(e)}
-            >
-              用户中心
-            </div>
-            <div className={this.state.sub2 ? 'menuList active' : 'menuList'}>
-              <div className="item">
-                <Link to='/inmanage/main/message' activeClassName='active'>Message</Link>
-              </div>
-              <div className="item">
-                <Link to='/inmanage/main/review' activeClassName='active'>不存在页面</Link>
-              </div>
-              <div className="item">
-                <Link to='/inmanage/main/review' activeClassName='active'>不存在页面</Link>
-              </div>
-              <div className="item">
-                <Link to='/inmanage/main/review' activeClassName='active'>不存在页面</Link>
-              </div>
-              <div className="item">
-                <Link to='/inmanage/main/review' activeClassName='active'>不存在页面</Link>
-              </div>
-            </div>
-          </div>
-        </div>
+        {Menu(this.state.menu)}
       </div>
     )
   }  
