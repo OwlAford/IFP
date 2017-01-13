afcb4e5d063b99c9e9b7b28113b023effa4b4ffb
OwlAford <1005476098@qq.com>
2017-01-13 15:11:42 +0800

    add welcome page

--- src/components/Welcome/WelcomeView.js
+++ src/components/Welcome/WelcomeView.js
@@ -4,32 +4,38 @@ import './Welcome.scss'
 export default class WelcomeView extends Component {
 
   render () {
-  	let main = this.props.main
+    let { currentCstIP, currentLoginTime, lastCstIP, lastLoginTime, loginCount } 
+        = this.props.main
     return (
       <div className="page-welcome">
         <div className="title">欢迎使用IFP内部管理系统！</div>
         <div className="table">
           <table>
-            <tr>
-              <td className="label">当前访问IP：</td>
-              <td>{main.currentCstIP}</td>
-            </tr>
-            <tr>
-              <td className="label">当前访问时间：</td>
-              <td>{main.currentLoginTime}</td>
-            </tr>
-            <tr>
-              <td className="label">最后一次访问IP：</td>
-              <td>{main.lastCstIP}</td>
-            </tr>
-            <tr>
-              <td className="label">最后一次登录时间：</td>
-              <td>{main.lastLoginTime}</td>
-            </tr>
-            <tr>
-              <td className="label">登录次数：</td>
-              <td>{main.loginCount}</td>
-            </tr>
+            {
+              [{
+                label: '当前访问IP：',
+                info: currentCstIP
+              },{
+                label: '当前访问时间：',
+                info: currentLoginTime
+              },{
+                label: '最后一次访问IP：',
+                info: lastCstIP
+              },{
+                label: '最后一次登录时间：',
+                info: lastLoginTime
+              },{
+                label: '登录次数：',
+                info: loginCount
+              }].map((item, i) => {
+                return (
+                  <tr key={i}>
+                    <td className="label">{item.label}</td>
+                    <td>{item.info}</td>
+                  </tr>
+                )
+              })
+            }
           </table>
         </div>
       </div>
