13544b953722d3683c8447484b6a6bb2e9bd0512
OwlAford <1005476098@qq.com>
2017-01-13 18:47:23 +0800

    add tbody

--- src/components/Welcome/WelcomeView.js
+++ src/components/Welcome/WelcomeView.js
@@ -11,31 +11,33 @@ export default class WelcomeView extends Component {
         <div className="title">欢迎使用IFP内部管理系统！</div>
         <div className="table">
           <table>
-            {
-              [{
-                label: '当前访问IP：',
-                info: currentCstIP
-              },{
-                label: '当前访问时间：',
-                info: currentLoginTime
-              },{
-                label: '最后一次访问IP：',
-                info: lastCstIP
-              },{
-                label: '最后一次登录时间：',
-                info: lastLoginTime
-              },{
-                label: '登录次数：',
-                info: loginCount
-              }].map((item, i) => {
-                return (
-                  <tr key={i}>
-                    <td className="label">{item.label}</td>
-                    <td>{item.info}</td>
-                  </tr>
-                )
-              })
-            }
+            <tbody>
+              {
+                [{
+                  label: '当前访问IP：',
+                  info: currentCstIP
+                },{
+                  label: '当前访问时间：',
+                  info: currentLoginTime
+                },{
+                  label: '最后一次访问IP：',
+                  info: lastCstIP
+                },{
+                  label: '最后一次登录时间：',
+                  info: lastLoginTime
+                },{
+                  label: '登录次数：',
+                  info: loginCount
+                }].map((item, i) => {
+                  return (
+                    <tr key={i}>
+                      <td className="label">{item.label}</td>
+                      <td>{item.info}</td>
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
           </table>
         </div>
       </div>
