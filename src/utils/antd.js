8987cb2aef081d82298b32eb853624c33b2e7680
Aford <244734834@qq.com>
2017-02-18 14:40:04 +0800

    Optimized package volume

--- src/utils/antd.js
+++ src/utils/antd.js
@@ -1,22 +1,58 @@
 import 'antd/lib/style'
-import 'antd/lib/alert/style/css'
-import 'antd/lib/button/style/css'
-import 'antd/lib/checkbox/style/css'
-import 'antd/lib/col/style/css'
-import 'antd/lib/date-picker/style/css'
-import 'antd/lib/form/style/css'
-import 'antd/lib/icon/style/css'
-import 'antd/lib/input/style/css'
-import 'antd/lib/message/style/css'
-import 'antd/lib/modal/style/css'
-import 'antd/lib/notification/style/css'
-import 'antd/lib/radio/style/css'
-import 'antd/lib/row/style/css'
-import 'antd/lib/select/style/css'
-import 'antd/lib/slider/style/css'
-import 'antd/lib/spin/style/css'
-import 'antd/lib/table/style/css'
-import 'antd/lib/tree/style/css'
-import 'antd/lib/tree-select/style/css'
 
-export { Alert, Button, Checkbox, Col, DatePicker, Form, Icon, Input, message, Modal, notification, Radio, Row, Select, Slider, Spin, Table, Tree, TreeSelect } from 'antd'
+export { default as Alert } from 'antd/lib/alert'
+import 'antd/lib/alert/style/css'
+
+export { default as Button } from 'antd/lib/button'
+import 'antd/lib/button/style/css'
+
+export { default as Checkbox } from 'antd/lib/checkbox'
+import 'antd/lib/checkbox/style/css'
+
+export { default as Col } from 'antd/lib/col'
+import 'antd/lib/col/style/css'
+
+export { default as DatePicker } from 'antd/lib/date-picker'
+import 'antd/lib/date-picker/style/css'
+
+export { default as Form } from 'antd/lib/form'
+import 'antd/lib/form/style/css'
+
+export { default as Icon } from 'antd/lib/icon'
+import 'antd/lib/icon/style/css'
+
+export { default as Input } from 'antd/lib/input'
+import 'antd/lib/input/style/css'
+
+export { default as message } from 'antd/lib/message'
+import 'antd/lib/message/style/css'
+
+export { default as Modal } from 'antd/lib/modal'
+import 'antd/lib/modal/style/css'
+
+export { default as notification } from 'antd/lib/notification'
+import 'antd/lib/notification/style/css'
+
+export { default as Radio } from 'antd/lib/radio'
+import 'antd/lib/radio/style/css'
+
+export { default as Row } from 'antd/lib/row'
+import 'antd/lib/row/style/css'
+
+export { default as Select } from 'antd/lib/select'
+import 'antd/lib/select/style/css'
+
+export { default as Slider } from 'antd/lib/slider'
+import 'antd/lib/slider/style/css'
+
+export { default as Spin } from 'antd/lib/spin'
+import 'antd/lib/spin/style/css'
+
+export { default as Table } from 'antd/lib/table'
+import 'antd/lib/table/style/css'
+
+export { default as Tree } from 'antd/lib/tree'
+import 'antd/lib/tree/style/css'
+
+export { default as TreeSelect } from 'antd/lib/tree-select'
+import 'antd/lib/tree-select/style/css'
