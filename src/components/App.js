fd7b1c5bf34d35d6e29f6fb5c25160505ac34bc3
Aford <244734834@qq.com>
2017-02-19 18:08:19 +0800

    uodate

--- src/components/App.js
+++ src/components/App.js
@@ -19,7 +19,7 @@ export default class App extends Component {
   	return (
       <Provider store={store}>
         <div className='app-wrap'>
-          <Router history={browserHistory} children={routes}/>
+          <Router history={browserHistory} routes={routes}/>
         </div>
       </Provider>
     )
