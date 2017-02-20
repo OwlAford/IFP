193ef418de69b156475dbb9ea9ab4d0b9ede1494
OwlAford <1005476098@qq.com>
2017-02-20 15:34:33 +0800

    change the babel stage

--- config/project.config.js
+++ config/project.config.js
@@ -23,7 +23,7 @@ const config = {
   compiler_babel : {
     cacheDirectory : true,
     plugins        : ['transform-runtime', 'transform-decorators-legacy'],
-    presets        : ['es2015', 'react', 'stage-2']
+    presets        : ['es2015', 'react', 'stage-0']
   },
   compiler_devtool         : 'source-map',
   compiler_hash_type       : 'hash',
