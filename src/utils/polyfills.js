8b03aa74aab41f5f654ac45d4cf74385ea0a944e
OwlAford <1005476098@qq.com>
2017-02-21 16:36:11 +0800

    update

--- src/utils/polyfills.js
+++ src/utils/polyfills.js
@@ -5,6 +5,6 @@ if (!window.Promise) {
   window.Promise = Promise
 }
 
-//判断是否IE的Edge浏览器
-const isEdge = navigator.userAgent.indexOf("Edge") > -1
-isEdge ? fetch = null : null
+// 判断是否IE的Edge浏览器，若是，覆盖MS Edge 自带 fetch 方法（此方法有缺陷，故替换）
+// fetch 方法的 polyfill 会判断浏览器是否有该属性，决定是否覆盖，故将 fetch 方法覆写为 null
+navigator.userAgent.indexOf('Edge') > -1 ? fetch = null : null
