7d9e23312f92e7a64a7280d6437dd636beadd07a
OwlAford <1005476098@qq.com>
2017-02-21 13:00:10 +0800

    fix edge request headers problems

--- src/utils/polyfills.js
+++ src/utils/polyfills.js
@@ -4,3 +4,7 @@ import './EventSource'
 if (!window.Promise) {  
   window.Promise = Promise
 }
+
+//判断是否IE的Edge浏览器
+const isEdge = navigator.userAgent.indexOf("Edge") > -1
+isEdge ? fetch = null : null
