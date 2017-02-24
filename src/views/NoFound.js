1183f0141c817b0539bd789dba31d60b20147d41
OwlAford <1005476098@qq.com>
2017-02-24 11:58:29 +0800

    update details

--- src/views/NoFound.js
+++ src/views/NoFound.js
@@ -2,17 +2,13 @@ import React, { Component, PropTypes } from 'react'
 import { HOME_PATH } from 'GLOBAL'
 import { message } from 'UTIL/antd'
 
-const error = function () {
-  message.error('找不到该页面！')
-}
-
 export default class NotFound extends Component {
   static contextTypes = {
     router: PropTypes.object.isRequired
   }
 
   componentWillMount() {
-    error()
+    message.error('找不到该页面！')
     let timer = setTimeout(() => {
       this.context.router.replace(HOME_PATH)
       clearTimeout(timer)
